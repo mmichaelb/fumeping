f4fd1a64a19642a8fab99d3048190ecb04a1696d
mmichaelb <mmichaelb@protonmail.com>
2021-01-07 12:37:51 +0100

    add influxdb handler

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
-	github.com/influxdata/influxdb-client-go/v2 v2.2.1 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.2.1
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
---
+++ pkg/influx/handler.go
@@ -0,0 +1,42 @@
+package influx
+
+import (
+	"context"
+	"fmt"
+	ping2 "github.com/go-ping/ping"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/mmichaelb/fumeping/pkg/ping"
+	"github.com/sirupsen/logrus"
+)
+
+const organization = "fumeping"
+const bucketSyntax = "stats-%s"
+
+type ResultHandler struct {
+	influxClient influxdb2.Client
+}
+
+func (handler *ResultHandler) Handle(host string, pinger ping2.Pinger, result ping.Result) {
+	logrus.WithField("host", host).Debug("storing ping result in influxdb")
+	api := handler.influxClient.WriteAPIBlocking(organization, fmt.Sprintf(bucketSyntax, host))
+	point := influxdb2.NewPointWithMeasurement("ping").
+		AddTag("host", host).
+		AddField("interval", pinger.Interval).
+		AddField("timeout", pinger.Timeout).
+		AddField("count", pinger.Count).
+		AddField("size", pinger.Size).
+		AddField("packetsRecv", result.PacketsRecv).
+		AddField("packetsSent", result.PacketsSent).
+		AddField("packetLoss", result.PacketLoss).
+		AddField("address", result.Addr).
+		AddField("minRtt", result.MinRtt).
+		AddField("maxRtt", result.MaxRtt).
+		AddField("avgRtt", result.AvgRtt).
+		AddField("stdDevRtt", result.StdDevRtt).
+		SetTime(result.Time)
+	if err := api.WritePoint(context.Background(), point); err != nil {
+		logrus.WithField("host", host).WithError(err).Errorln("Could not write ping result point to InfluxDB")
+		return
+	}
+	logrus.WithField("host", host).Debug("Successfully written ping result point to InfluxDB")
+}
